7ab01a6c38e19b59020fc3deef28d4b42c20b66a
Your Name <you@example.com>
2018-08-12 11:28:45 +0700

    abc

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ mc =minecraft.Minecraft.create("10.15.0.75")
 mc.postToChat("hello")
 pl = Player.MinePLayer(mc,"123")
 x,y,z = pl.getPos()
-pl.setPos(100,100,100)
+
 Goods.bed(mc,x,y,z)
 Goods.table(mc,x,y,z)
-#Nature.lake(mc,x,y,z)
+
