c522dae39a795ea10331e26e9c2fbd68779b074e
Your Name <you@example.com>
2018-08-12 11:25:44 +0700

    ok

--- lib/Nature.py
+++ lib/Nature.py
@@ -20,12 +20,11 @@ def treeKien(mc,x,y,z):
     mc.setBlock(x+3,y+6,z+3,0)
     mc.setBlocks(x+2,y,z+2,x+2,y+4,z+2,17)
 def lake(mc,x,y,z):
-    mc.setBlocks(x,y,z,x+20,y+15,z+20,2)
-    mc.setBlocks(x+12,y+10,z+4,x+8,y+10,z+4,8)
-    mc.setBlocks(x+13,y+10,z+5,x+7,y+10,z+5,8)
-    mc.setBlocks(x+14,y+10,z+6,x+6,y+10,z+6,8)
-    mc.setBlocks(x+15,y+10,z+7,x+5,y+10,z+7,8)
-    mc.setBlocks(x+16,y+10,z+8,x+4,y+10,z+8,8)
-    
+    for i in range(10):
+        mc.setBlocks(x - random.randint(3,5),y,z+i
+        ,x + random.randint(3,5),y+10,z+i,46)
 
 
+def volcano(mc,x,y,z):
+    for i in range(10):
+        mc.setBlocks(x+1+i,y,z+1+i,x+31-i,y,z+31-i,1)
