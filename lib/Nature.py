8862376d167e85cf334835ea1ad0454517a2ff4a
huyhuy17-site <huynguyenquang@enbac.com>
2018-08-12 11:53:18 +0700

    river

--- lib/Nature.py
+++ lib/Nature.py
@@ -40,3 +40,7 @@ def forrest(mc,x,y,z):
     for i in range (0,15,5):
         for j in range (0,15,5):
             tree(mc,x-5+ i,y,z -5 +j,)
+
+def river (mc,x,y,z):
+    mc.setBlocks(x,y,z ,x+10 ,y-2,z+10 ,7)
+    mc.setBlock(x+5,y-1,z+ 5, x+6, y,z+ 6 ,1)
