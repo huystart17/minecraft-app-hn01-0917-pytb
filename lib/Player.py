38856ac36e552ec176c4beb67085ad5219ce7cff
Nguyễn Quang Huy <huy.start17@gmail.com>
2018-08-18 19:11:22 +0700

    Update Player.py

--- lib/Player.py
+++ lib/Player.py
@@ -1,8 +1,11 @@
 class MinePLayer ():
-  def __init__(self, minecraft_connect, player_name):
+  def __init__(self, minecraft_connect, player_name='unname', id =''):
     self.conn = minecraft_connect
     self.name = player_name
-    self.id = minecraft_connect.getPlayerEntityId(player_name)
+    if(len(str(id)) > 0):
+      self.id = id
+    else:
+      self.id = minecraft_connect.getPlayerEntityId(player_name)
 
   def setPos (self,x,y,z):
     sc = self.conn
@@ -13,8 +16,9 @@ class MinePLayer ():
     x,y,z = sc.entity.getPos(self.id)
     return (x,y,z)
 
-  def getMySight(self):
+  def getMySight(self,length =3 ):
     sc = self.conn
     x,y,z = sc.entity.getPos(self.id)
     x1, y1, z1 = sc.entity.getDirection(self.id)
-    return (x + x1 * 2, y + y1* 2, z +z1*2 )
+    return (x + x1 * length, y + y1* length, z +z1*length )
+
