19e5bfb818c084155292fd725758e9cdaaf7992d
huyhuy17-site <huynguyenquang@enbac.com>
2018-08-19 10:40:55 +0700

    chat command

--- building_command.py
+++ building_command.py
@@ -4,6 +4,15 @@ from lib import Player
 from lib import Goods
 from lib import Nature
 
-mc =minecraft.Minecraft.create('10.15.0.194')
+mc = minecraft.Minecraft.create('10.15.0.194')
 mc.postToChat("hello")
-pl = Player.MinePLayer(mc,"mai")
+pl = Player.MinePLayer(mc, "aaaaa")
+
+while True:
+    chatEvents = mc.events.pollChatPosts()
+    for chatevent in chatEvents:
+        if (chatevent.message == "pillar"):
+            pl = Player.MinePLayer(minecraft_connect=mc, id=chatevent.entityId)
+            x, y, z = pl.getMySight(10)
+            Building.pillars(mc, x, y, z)
+            mc.events.clearAll()
