6d3f600ff010098e6f4b3014f756044becea8b83
huyhuy17-site <huynguyenquang@enbac.com>
2018-08-19 10:26:00 +0700

    cap nhat

---
+++ building_command.py
@@ -0,0 +1,9 @@
+from mcpi import minecraft
+from lib import Building
+from lib import Player
+from lib import Goods
+from lib import Nature
+
+mc =minecraft.Minecraft.create('10.15.0.194')
+mc.postToChat("hello")
+pl = Player.MinePLayer(mc,"mai")
