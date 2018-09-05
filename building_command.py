9b87ec43df70b7417faf7fe336b7fdd6a2f325b1
ha <chuhoangha016@gmail.com>
2018-09-05 11:37:00 +0700

    ok

--- building_command.py
+++ building_command.py
@@ -1,17 +1,32 @@
 from mcpi import minecraft
-from lib import Building
-from lib import Player
-from lib import Goods
-from lib import Nature
-from lib import SimpleBuilding
+import time
 from lib import Game
+from lib import Player
 mc =minecraft.Minecraft.create()
-
+a=0
 while True:
     chats=mc.events.pollChatPosts()
     for chat in chats:
-        if chat.message!='':
+        if chat.message=='game':
             pl=Player.MinePLayer(minecraft_connect=mc,id=chat.entityId)
             x,y,z=pl.getMySight(15)
             Game.arena(mc,x,y,z)
             print(chat)
+            a=1
+            break
+    if a==1:
+        break
+
+print('aaaaaa')
+ids=mc.getPlayerEntityIds()
+pls=[]
+for id in ids:
+    pl=Player.MinePLayer(minecraft_connect=mc,id=id)
+    pls.append(pl)
+
+while True:
+    time.sleep(4)
+    for p in pls:
+        x,y,z=p.getPos()
+        if mc.getBlock(x,y-1,z)==57:
+            mc.setBlock(x,y-1,z,0)
